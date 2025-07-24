479a3fbb65af02cecdced64d9a5320ad72245a31
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-24 17:22:38 +0530

    Update navya_set_elements.py

--- navya_set_elements.py
+++ navya_set_elements.py
@@ -2,4 +2,4 @@ A = {1, 2, 3, 4, 5}
 B = {3, 4, 5, 6, 7}
 C = {5, 6, 7, 8, 9}
 D = {1, 3, 7, 9, 10}
-print(((A & B)|(A & C)|(A & D)|(B & C)|(C & D)) - ((A & B & C)|(A & B & D)|(A & C & D)|(B & C & D)))
+print("Elements in exactly 2 sets:",((A & B)|(A & C)|(A & D)|(B & C)|(C & D)) - ((A & B & C)|(A & B & D)|(A & C & D)|(B & C & D)))
